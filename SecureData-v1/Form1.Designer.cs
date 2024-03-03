569daacc3b46be060568c0cd9e3ebc6e62faa56f
tltrus <77125487+tltrus@users.noreply.github.com>
2024-03-03 18:26:31 +0400

    up

---
+++ SecureData-v1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SecureInfoProg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.btnNewFile = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbPassw = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb.Location = new System.Drawing.Point(6, 6);
+            this.rtb.Margin = new System.Windows.Forms.Padding(2);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(287, 328);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // btnNewFile
+            // 
+            this.btnNewFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewFile.Location = new System.Drawing.Point(316, 13);
+            this.btnNewFile.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNewFile.Name = "btnNewFile";
+            this.btnNewFile.Size = new System.Drawing.Size(106, 25);
+            this.btnNewFile.TabIndex = 1;
+            this.btnNewFile.Text = "Clear";
+            this.btnNewFile.UseVisualStyleBackColor = true;
+            this.btnNewFile.Click += new System.EventHandler(this.btnNewFile_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenFile.Location = new System.Drawing.Point(316, 43);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(106, 25);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Open file";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Location = new System.Drawing.Point(316, 73);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(106, 25);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveAs.Location = new System.Drawing.Point(316, 103);
+            this.btnSaveAs.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(106, 25);
+            this.btnSaveAs.TabIndex = 1;
+            this.btnSaveAs.Text = "Save as...";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Enabled = false;
+            this.btnDecrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDecrypt.Location = new System.Drawing.Point(10, 35);
+            this.btnDecrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(106, 25);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tbPassw);
+            this.panel1.Controls.Add(this.btnDecrypt);
+            this.panel1.Location = new System.Drawing.Point(430, 13);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(135, 77);
+            this.panel1.TabIndex = 2;
+            // 
+            // tbPassw
+            // 
+            this.tbPassw.Enabled = false;
+            this.tbPassw.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.tbPassw.Location = new System.Drawing.Point(10, 12);
+            this.tbPassw.Margin = new System.Windows.Forms.Padding(2);
+            this.tbPassw.Name = "tbPassw";
+            this.tbPassw.Size = new System.Drawing.Size(108, 20);
+            this.tbPassw.TabIndex = 2;
+            this.tbPassw.Text = "введите пароль";
+            this.tbPassw.UseSystemPasswordChar = true;
+            this.tbPassw.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbPassw_MouseClick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.Controls.Add(this.rtb);
+            this.panel2.Location = new System.Drawing.Point(7, 7);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(301, 340);
+            this.panel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 354);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnNewFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SecureData v1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Button btnNewFile;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbPassw;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ SecureData-v1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SecureInfoProg
+{
+    public partial class Form1 : Form
+    {
+        string currentFileName;
+        const string SharedSecret = "dsfsf/saasde/vfsfEd A physics simu%lation wo!rks 4522e02l!4d933FAV4dby making many smal%l predi()ctions based on the laws of physics(!)4Fsl6NXeap";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "SecureData files(*.sec)|*.sec|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "SecureData files(*.sec)|*.sec|All files(*.*)|*.*";
+        }
+
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            currentFileName = openFileDialog1.FileName;
+            btnDecrypt.Enabled = true;
+            btnSave.Enabled = true;
+            tbPassw.Enabled = true;
+
+            rtb.Clear();
+            using (FileStream fstream = File.OpenRead(currentFileName))
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+
+                rtb.AppendText(textFromFile);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var encryptedStringAES = Crypto.EncryptStringAES(rtb.Text, SharedSecret);
+
+                using (FileStream fstream = new FileStream(currentFileName, FileMode.Create))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(encryptedStringAES);
+                    fstream.Write(array, 0, array.Length);
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            // шифрование
+            var encryptedStringAES = Crypto.EncryptStringAES(rtb.Text, SharedSecret);
+
+            // сохранить как... в файл
+            using (FileStream fstream = new FileStream(saveFileDialog1.FileName, FileMode.Create))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(rtb.Text);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+            }
+        }
+
+        private void btnNewFile_Click(object sender, EventArgs e) => rtb.Clear();
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (tbPassw.Text == "") return;
+            string pattern = Crypto.DecryptStringAES("EAAAAKd9zzKYMavc4LeCN916UJMte6VXdfjf4nlqFqcUwQBV", SharedSecret); // password is qwrty
+
+            if (pattern == tbPassw.Text)
+            {
+                string text = Crypto.DecryptStringAES(rtb.Text, SharedSecret);
+
+                tbPassw.Text = "";
+                rtb.Clear();
+                rtb.AppendText(text);
+
+                btnDecrypt.Enabled = false;
+                tbPassw.Enabled = false;
+            }
+        }
+
+        private void tbPassw_MouseClick(object sender, MouseEventArgs e) => tbPassw.Text = "";
+    }
+}
